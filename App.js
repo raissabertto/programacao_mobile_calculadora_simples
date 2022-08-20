4b4995f462d3b87bcbdf08d6c14a68fbd06deefd
raissabertto <raissaberto7@gmail.com>
2022-08-20 01:44:43 +0000

    cona soma

--- App.js
+++ App.js
@@ -1,24 +1,47 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
+	const [valor1, setValor1] = useState(20);
+	const [valor2, setValor2] = useState(20);
+	const [resultado, setResultado] = useState();
+	function somar(){
+		let r = parseFloat(valor1) + parseFloat(valor2);
+		setResultado(r);
+	}
+
   return (
     <View style={styles.container}>
 		  <View style={styles.bloco}>
-			  <Text>Calculadora Simples</Text>
+			  <Text style={styles.titulo}>Calculadora Simples</Text>
 		  </View>
 		  <View style={styles.bloco}>
 		  	<Text>Valor 1: </Text>
-			  <TextInput style = {styles.input}/>
+			  <TextInput 
+				style = {styles.input}
+				value={valor1}
+				  onChangeText={(valor)=>setValor1(valor)}
+				  keyboardType="numeric"
+				/>
 		  </View>
 		  <View style={styles.bloco}>
 		  	<Text>Valor 2: </Text>
-			  <TextInput style = {styles.input}/>
+			  <TextInput 
+				style = {styles.input}
+				value={valor2}
+				  onChangeText={(valor)=>setValor2(valor)}
+				  keyboardType="numeric"
+				/>
 		  </View>
 		  <View style={styles.bloco}>
-		  	<TouchableOpacity style = {styles.botao}>
+		  	<TouchableOpacity style = {styles.botao}
+				onPress={somar}
+				>
 			 	<Text style = {styles.textoBotao}>Somar</Text> 
 			</TouchableOpacity>
+		  </View>
+		  <View style={styles.bloco}>
+			  <Text>Resultado: {resultado}</Text>
 		  </View>
     </View>
   );
